0c9e6d11f7cfab1103f83f196735cc20b2002138
Zafar <zafonline23@gmail.com>
2024-10-14 12:43:29 +0300

    new hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,8 +7,8 @@ export const Hero = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hello, I am Zafar</h1>
-            <p className={styles.description}>I am a React front-end developer with a lot of experience in web development reach out if you are interested.</p>
-            <a href="mailto:mygmail@gmail.com" className={styles.contactBtn}>Contact me</a>
+            <p className={styles.description}>I am a web developer specialized in front-end development using React.js.</p>
+            <a href="mailto:zafonline23@gmail.com" className={styles.contactBtn}>Contact me</a>
         </div>
         <img src={getImageUrl("hero/React.webp")} alt="My image" className={styles.heroImg}/>
         <div className={styles.topBlur}/>
